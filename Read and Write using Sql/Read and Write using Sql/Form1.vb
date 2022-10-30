489001a61d4bdce011fea5b97099555c7a08f208
MARIMUTHU <116822610+marimuthu2000@users.noreply.github.com>
2022-10-30 13:18:42 +0530

    Add files via upload

---
+++ Read and Write using Sql/Read and Write using Sql/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports System.Data
+Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim con As SqlConnection = New SqlConnection("Data Source=LAPTOP-OJC1HEI8\SQLEXPRESS;Initial Catalog=DEMO;Integrated Security=True")
+            con.Open()
+            Dim cmd As New SqlCommand("select NAME,ID,POSITION from Emp_detail where NAME=@NAME", con)
+            cmd.Parameters.AddWithValue("NAME", TextBox1.Text)
+            Dim myreader As SqlDataReader
+            myreader = cmd.ExecuteReader
+            If myreader.Read() Then
+                TextBox1.Text = myreader("NAME")
+                TextBox2.Text = myreader("ID")
+                TextBox3.Text = myreader("POSITION")
+            Else
+                MessageBox.Show("No Data Found")
+            End If
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim con As SqlConnection = New SqlConnection("Data Source=LAPTOP-OJC1HEI8\SQLEXPRESS;Initial Catalog=DEMO;Integrated Security=True")
+            Dim cmd1 As New SqlCommand
+        Dim myreader As SqlDataReader
+        Try
+            con.Open()
+            Dim query As String
+            query = "insert into Emp_detail(NAME,ID,POSITION) values ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "') "
+            cmd1 = New SqlCommand(query, con)
+            myreader = cmd1.ExecuteReader
+            If myreader.Read() Then
+                TextBox1.Text = myreader("NAME")
+                TextBox2.Text = myreader("ID")
+                TextBox3.Text = myreader("POSITION")
+            Else
+                MessageBox.Show("Fill the Data")
+            End If
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+End Class
---
+++ Read and Write using Sql/Read and Write using Sql/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Read and Write using Sql")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Read and Write using Sql")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0b7ddaae-14c7-4215-ba8d-ec318e176ac6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
